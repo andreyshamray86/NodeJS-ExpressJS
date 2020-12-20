12aa6860b1111597d06e6ea8ec6f00fd38c16b15
Andrey <andreyshamray86@gmail.com>
2020-12-20 19:20:22 +0300

    Added order page functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
 const homeRoutes = require('./routes/home');
+const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 
@@ -15,13 +16,14 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
     extended: true
 }));
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
 
 
 app.get("/courses", (req, res) => {
