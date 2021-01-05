17fad7e8e52cea5cb26cb0abceedca0976cecbf6
Andrey <andreyshamray86@gmail.com>
2021-01-05 15:33:37 +0300

    Added Order page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
+const orderRoutes = require('./routes/order');
 const coursesRoutes = require('./routes/courses');
 const User = require('./models/user');
 
@@ -40,6 +41,7 @@ app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
+app.use('/order', orderRoutes);
 
 
 app.get("/courses", (req, res) => {
