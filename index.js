98dffdb95e9950eced74c0dbd75cbff1b8844c88
Andrey <andreyshamray86@gmail.com>
2021-01-05 14:31:10 +0300

    refactoring Cart

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const User = require('./models/user');
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -20,6 +21,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5ff43ca192b1b84d6c99cd68');
+        req.user = user;
+        next();
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
@@ -48,6 +59,20 @@ async function start() {
             useFindAndModify: false,
             useUnifiedTopology: true
         });
+
+        const candidate = await User.findOne();
+
+        if (!candidate) {
+            const user = new User({
+                email: 'andrey@mail.ru',
+                name: 'Andrey',
+                cart: {
+                    items: []
+                }
+            });
+            await user.save();
+        }
+
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
